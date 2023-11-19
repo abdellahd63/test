87b474499091162f96575f912732319062e3668e
Abdallah DEKKICHE <119437598+abdellahd63@users.noreply.github.com>
2023-11-19 18:29:52 +0100

    db string

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const app=express();
 const mongoose= require('mongoose')
-const dbURI='mongodb+srv://karim:test1234@karim.9cq8aes.mongodb.net/?retryWrites=true&w=majority';
+
+
+const dbURI='mongodb+srv://karim00:test1234@karim.9cq8aes.mongodb.net/karimdatabase';
+//const dbURI='mongodb+srv://usernameDBtest:CdSteeJMJpg4qOuW@cluster0.hibcrcx.mongodb.net/Karim'
+
 mongoose.connect(dbURI)
 .then((result)=> console.log('db connected'))
 .catch((err) => {
